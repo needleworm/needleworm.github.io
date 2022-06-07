1f9ae31801fe726317a41ab7b3be23c7c3ac45f9
needleworm <bhban@kakao.com>
2022-06-07 17:31:06 +0900

    .

--- src/components/collections.js
+++ src/components/collections.js
@@ -20,39 +20,78 @@ class Collections extends Component {
   }
 
   drawWebsites(){
-    var reactIconL = <i className="fab fa-react"></i>
-    var gitLogo = <i className="fab fa-github"></i>
-
     var collection1 = <div className="singleProjectContainer">
       <div className="singleWebsiteCard">
         <img className="websiteImage" src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/collection/1.jpeg" alt="projectimage"/>
         <div className="projectText">
-          <h5 className="text-center textMarginBottom">
-            The Inner Sky - Orange in you
-          </h5>
+          <div className="text-center textMarginBottom">
+            <strong>
+              The Inner Sky - Orange in you
+            </strong>
+          </div>
           <div className="codeBody">
             <p className="bookDescription">
-              My own webpage built with &nbsp;{reactIconL} React. This static website is hosted on &nbsp;{gitLogo} Github Pages.
-              It was inspired by &nbsp; 
-              <a href='https://github.com/tzuehlke/jekyll-uno-timeline/' target="_blank"  rel="noreferrer">
-                Thomas Zühlke's jekyll theme
-              </a>. The original theme runs with jQuery for github API calling, and SCSS for styling. 
-              And its jQuery function supported only one content wrapper element.<br/>
-              I built a  &nbsp;{reactIconL} React app looks similar to Thomas' work. 
-              As you can see, this page supports multiple content wrapper elements. 
-              If you are interested in using this site theme to build your own page, please visit my repository to get the source code.
+              Oil on Canvas, 40.9 x 53 cm<br/>
+              <a href="https://nonez.creatorlink.net/" target="_blank" rel="noreferrer">
+                <strong>넌지</strong> (2022)
+              </a><br/>
             </p>
+            <div className="collectedFrom">
+              Collected from <a className="collectedFrom" href="https://mansion9.co.kr/" target="_blank" rel="noreferrer">
+                Manshion 9
+                </a>
+            </div>
           </div>
-          <div className="projectIcons">
-            <h5>{reactIconL}&nbsp;React&nbsp;&nbsp;{gitLogo}&nbsp;Pages</h5>
-            <div className="gitIcons">
-              <a href="https://github.com/needleworm/needleworm.github.io" target="_blank"  rel="noreferrer">
-                <i className="fas fa-code"></i>
-              </a>
-              &nbsp;&nbsp;&nbsp;&nbsp;
-              <a href="https://needleworm.github.io" target="_blank"  rel="noreferrer">
-                <i className="fas fa-external-link-alt"></i>
-              </a>
+        </div>
+      </div>
+    </div>
+
+    var collection2 = <div className="singleProjectContainer">
+      <div className="singleWebsiteCard">
+        <img className="websiteImage" src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/collection/2.jpeg" alt="projectimage"/>
+        <div className="projectText">
+          <div className="text-center textMarginBottom">
+            <strong>
+              The Inner Sky - Lens-scape
+            </strong>
+          </div>
+          <div className="codeBody">
+            <p className="bookDescription">
+              Oil on Canvas, 40.9 x 53 cm<br/>
+              <a href="https://nonez.creatorlink.net/" target="_blank" rel="noreferrer">
+                <strong>넌지</strong> (2022)
+              </a><br/>
+            </p>
+            <div className="collectedFrom">
+              Collected from <a className="collectedFrom" href="https://www.art9gallery.net/" target="_blank" rel="noreferrer">
+                아트나인갤러리
+                </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    var collection3 = <div className="singleProjectContainer">
+      <div className="singleWebsiteCard">
+        <img className="websiteImage" src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/collection/3.jpeg" alt="projectimage"/>
+        <div className="projectText">
+          <div className="text-center textMarginBottom">
+            <strong>
+              The Inner Sky - Enjoy
+            </strong>
+          </div>
+          <div className="codeBody">
+            <p className="bookDescription">
+              Oil on Canvas, 40.9 x 53 cm<br/>
+              <a href="https://nonez.creatorlink.net/" target="_blank" rel="noreferrer">
+                <strong>넌지</strong> (2021)
+              </a><br/>
+            </p>
+            <div className="collectedFrom">
+              Collected from <a className="collectedFrom" href="https://www.k-auction.com/" target="_blank" rel="noreferrer">
+                K Auction
+                </a>
             </div>
           </div>
         </div>
@@ -60,9 +99,12 @@ class Collections extends Component {
     </div>
 
 
+
     return (
       <div className="websitesContainer">
         <div className="text-center textMarginBottom" style={{marginBottom:"200px"}}>
+          {collection3}
+          {collection2}
           {collection1}
         </div>
       </div>
