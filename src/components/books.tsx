7ef9116b17cccaca2eb7b4faf446d512e123204f
needleworm <bhban@kakao.com>
2023-01-17 12:49:50 +0900

    .

--- src/components/books.tsx
+++ src/components/books.tsx
@@ -10,56 +10,15 @@ function Books() {
   const ridiIcon = <img src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/stores/ridi.jpg" className="bookStoreIcon" alt="ridibooks"/>
 
   const drawBooks = () => {
-    let bookSection1 = <div className="singleBookContainer">
-      <img src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/covers/1.jpg" alt="book1" className="bookCoverImage"/>
-      <h5><a href="https://needleworm.github.io/bhban_rpa" target="_blank"  rel="noreferrer">6개월 치 업무를 하루 만에 끝내는 업무자동화 <i className="fas fa-external-link-alt"></i></a></h5>
-      <p className="bookDescription">2020.12. 생능출판사</p>
-      <ul className="list-inline">
-        <li><a href="https://www.aladin.co.kr/shop/wproduct.aspx?ISBN=8970504745" target="_blank" rel="noreferrer">{alladinIcon}</a></li>
-        <li><a href="https://product.kyobobook.co.kr/detail/S000001075971" target="_blank"  rel="noreferrer">{kyoboIcon}</a></li>
-        <li><a href="https://search.shopping.naver.com/book/catalog/32445091399" target="_blank"  rel="noreferrer">{naverIcon}</a></li>
-        <li><a href="http://www.yes24.com/Product/Goods/96360166" target="_blank"  rel="noreferrer">{yes24Icon}</a></li>
-      </ul>
-    </div>
 
-    let bookSection2 = <div className="singleBookContainer">
-      <img src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/covers/2.jpg" alt="book2" className="bookCoverImage"/>
-      <h5>공학자의 오경묵상</h5>
-      <p className="bookDescription">2020.11. BOOKK(부크크)</p>
-      <ul className="list-inline">
-        <li><a href="https://www.aladin.co.kr/shop/wproduct.aspx?ISBN=K112735243" target="_blank"  rel="noreferrer">{alladinIcon}</a></li>
-        <li><a href="https://product.kyobobook.co.kr/detail/S000060613527" target="_blank"  rel="noreferrer">{kyoboIcon}</a></li>
-        <li><a href="https://search.shopping.naver.com/book/catalog/32490302839" target="_blank"  rel="noreferrer">{naverIcon}</a></li>
-        <li><a href="http://www.yes24.com/Product/Goods/95590656" target="_blank"  rel="noreferrer">{yes24Icon}</a></li>
-      </ul>
+    let bookSection0 = <div className="singleBookContainer">
+      <img src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/covers/0.jpg" alt="book2" className="bookCoverImage"/>
+      <h5>환세취호전</h5>
+      <p className="bookDescription">2004.방학숙제</p>
     </div>
     
-    let bookSection3 = <div className="singleBookContainer">
-      <img src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/covers/3.jpg" alt="book3" className="bookCoverImage"/>
-      <h5>공학자의 지혜묵상</h5>
-      <p className="bookDescription">2020.05. BOOKK(부크크)</p>
-      <ul className="list-inline">
-        <li><a href="https://www.aladin.co.kr/shop/wproduct.aspx?ISBN=K452639642" target="_blank"  rel="noreferrer">{alladinIcon}</a></li>
-        <li><a href="https://product.kyobobook.co.kr/detail/S000060611595" target="_blank"  rel="noreferrer">{kyoboIcon}</a></li>
-        <li><a href="https://search.shopping.naver.com/book/catalog/32485690654" target="_blank"  rel="noreferrer">{naverIcon}</a></li>
-        <li><a href="http://www.yes24.com/Product/Goods/90392271" target="_blank"  rel="noreferrer">{yes24Icon}</a></li>
-      </ul>
-    </div>
-    
-    let bookSection4 = <div className="singleBookContainer">
-      <img src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/covers/4.jpg" alt="book4" className="bookCoverImage"/>
-      <h5>나는 아직 잊힐 준비가 되지 않았어요</h5>
-      <p className="bookDescription">2020.08. BOOKK(부크크)</p>
-      <ul className="list-inline">
-        <li><a href="https://www.aladin.co.kr/shop/wproduct.aspx?ISBN=K622632023" target="_blank"  rel="noreferrer">{alladinIcon}</a></li>
-        <li><a href="https://product.kyobobook.co.kr/detail/S000060612313" target="_blank"  rel="noreferrer">{kyoboIcon}</a></li>
-        <li><a href="https://search.shopping.naver.com/book/catalog/32506471953" target="_blank"  rel="noreferrer">{naverIcon}</a></li>
-        <li><a href="http://www.yes24.com/Product/Goods/92154589" target="_blank"  rel="noreferrer">{yes24Icon}</a></li>
-      </ul>
-    </div>
-    
-    let bookSection5 = <div className="singleBookContainer">
-      <img src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/covers/5.jpg" alt="book5" className="bookCoverImage"/>
+    let bookSection1 = <div className="singleBookContainer">
+      <img src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/covers/1.jpg" alt="book5" className="bookCoverImage"/>
       <h5>법대로 합시다</h5>
       <p className="bookDescription">2016.02. 지식과감성#</p>
       <ul className="list-inline">
@@ -68,10 +27,10 @@ function Books() {
       </ul>
     </div>
     
-    let bookSection6 = <div className="singleBookContainer">
-      <img src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/covers/6.jpg" alt="book6" className="bookCoverImage"/>
+    let bookSection2 = <div className="singleBookContainer">
+      <img src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/covers/2.jpg" alt="book6" className="bookCoverImage"/>
       <h5>실전 민사소송법</h5>
-      <p className="bookDescription">2020.05. BOOKK(부크크)</p>
+      <p className="bookDescription">2017.05. 해피로라</p>
       <ul className="list-inline">
         <li><a href="https://www.aladin.co.kr/shop/wproduct.aspx?ISBN=K572639625" target="_blank"  rel="noreferrer">{alladinIcon}</a></li>
         <li><a href="https://product.kyobobook.co.kr/detail/S000060611461" target="_blank"  rel="noreferrer">{kyoboIcon}</a></li>
@@ -80,8 +39,8 @@ function Books() {
       </ul>
     </div>
 
-    let bookSection7 = <div className="singleBookContainer">
-      <img src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/covers/7.jpg" alt="book7" className="bookCoverImage"/>
+    let bookSection3 = <div className="singleBookContainer">
+      <img src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/covers/3.jpg" alt="book7" className="bookCoverImage"/>
       <h5>코딩하는 공익</h5>
       <p className="bookDescription">2020.04. 세창출판사</p>
       <ul className="list-inline">
@@ -92,6 +51,54 @@ function Books() {
       </ul>
     </div>
     
+    let bookSection4 = <div className="singleBookContainer">
+      <img src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/covers/43.jpg" alt="book3" className="bookCoverImage"/>
+      <h5>공학자의 지혜묵상</h5>
+      <p className="bookDescription">2020.05. BOOKK(부크크)</p>
+      <ul className="list-inline">
+        <li><a href="https://www.aladin.co.kr/shop/wproduct.aspx?ISBN=K452639642" target="_blank"  rel="noreferrer">{alladinIcon}</a></li>
+        <li><a href="https://product.kyobobook.co.kr/detail/S000060611595" target="_blank"  rel="noreferrer">{kyoboIcon}</a></li>
+        <li><a href="https://search.shopping.naver.com/book/catalog/32485690654" target="_blank"  rel="noreferrer">{naverIcon}</a></li>
+        <li><a href="http://www.yes24.com/Product/Goods/90392271" target="_blank"  rel="noreferrer">{yes24Icon}</a></li>
+      </ul>
+    </div>
+    
+    let bookSection5 = <div className="singleBookContainer">
+      <img src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/covers/5.jpg" alt="book4" className="bookCoverImage"/>
+      <h5>나는 아직 잊힐 준비가 되지 않았어요</h5>
+      <p className="bookDescription">2020.08. BOOKK(부크크)</p>
+      <ul className="list-inline">
+        <li><a href="https://www.aladin.co.kr/shop/wproduct.aspx?ISBN=K622632023" target="_blank"  rel="noreferrer">{alladinIcon}</a></li>
+        <li><a href="https://product.kyobobook.co.kr/detail/S000060612313" target="_blank"  rel="noreferrer">{kyoboIcon}</a></li>
+        <li><a href="https://search.shopping.naver.com/book/catalog/32506471953" target="_blank"  rel="noreferrer">{naverIcon}</a></li>
+        <li><a href="http://www.yes24.com/Product/Goods/92154589" target="_blank"  rel="noreferrer">{yes24Icon}</a></li>
+      </ul>
+    </div>
+
+    let bookSection6 = <div className="singleBookContainer">
+      <img src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/covers/6.jpg" alt="book2" className="bookCoverImage"/>
+      <h5>공학자의 오경묵상</h5>
+      <p className="bookDescription">2020.11. BOOKK(부크크)</p>
+      <ul className="list-inline">
+        <li><a href="https://www.aladin.co.kr/shop/wproduct.aspx?ISBN=K112735243" target="_blank"  rel="noreferrer">{alladinIcon}</a></li>
+        <li><a href="https://product.kyobobook.co.kr/detail/S000060613527" target="_blank"  rel="noreferrer">{kyoboIcon}</a></li>
+        <li><a href="https://search.shopping.naver.com/book/catalog/32490302839" target="_blank"  rel="noreferrer">{naverIcon}</a></li>
+        <li><a href="http://www.yes24.com/Product/Goods/95590656" target="_blank"  rel="noreferrer">{yes24Icon}</a></li>
+      </ul>
+    </div>
+
+    let bookSection7 = <div className="singleBookContainer">
+      <img src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/covers/7.jpg" alt="book1" className="bookCoverImage"/>
+      <h5><a href="https://needleworm.github.io/bhban_rpa" target="_blank"  rel="noreferrer">6개월 치 업무를 하루 만에 끝내는 업무자동화 <i className="fas fa-external-link-alt"></i></a></h5>
+      <p className="bookDescription">2020.12. 생능출판사</p>
+      <ul className="list-inline">
+        <li><a href="https://www.aladin.co.kr/shop/wproduct.aspx?ISBN=8970504745" target="_blank" rel="noreferrer">{alladinIcon}</a></li>
+        <li><a href="https://product.kyobobook.co.kr/detail/S000001075971" target="_blank"  rel="noreferrer">{kyoboIcon}</a></li>
+        <li><a href="https://search.shopping.naver.com/book/catalog/32445091399" target="_blank"  rel="noreferrer">{naverIcon}</a></li>
+        <li><a href="http://www.yes24.com/Product/Goods/96360166" target="_blank"  rel="noreferrer">{yes24Icon}</a></li>
+      </ul>
+    </div>
+
     let bookSection8 = <div className="singleBookContainer">
       <img src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/books/covers/8.jpg" alt="book8" className="bookCoverImage"/>
       <h5><a href="https://needleworm.github.io/bhban_ai" target="_blank"  rel="noreferrer">쉬운 딥러닝 <i className="fas fa-external-link-alt"></i></a></h5>
@@ -248,13 +255,14 @@ function Books() {
         {bookSection10}
         {bookSection9}
         {bookSection8}
-        {bookSection1}
         {bookSection7}
+        {bookSection6}
+        {bookSection5}
+        {bookSection4}
         {bookSection3}
         {bookSection2}
-        {bookSection6}
-        {bookSection4}
-        {bookSection5}
+        {bookSection1}
+        {bookSection0}
       </div>
     )
   }
