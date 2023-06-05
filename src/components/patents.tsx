b5c08eb86545f1db055b928d58f5880611efa527
needleworm <bhban@kakao.com>
2023-06-05 10:04:42 +0900

    .

--- src/components/patents.tsx
+++ src/components/patents.tsx
@@ -1326,6 +1326,51 @@ function Patents() {
       </div>
     </div>
 
+    let patent30 = <div className="singleProjectContainer">
+      <div className="singleWebsiteCard">
+        <img className="websiteImage" src="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/images/patents/30.jpg" alt="projectimage"/>
+        <div className="projectText">
+          <h5>3차원 영상 재건 알고리즘을 활용한 식물공장 디지털 트윈 인터페이스 제공 장치 및 방법</h5><br/>
+          <div className="bookDescription">
+            <strong>등록번호</strong> &nbsp;&nbsp;&nbsp;10-2538747
+            <div className="patentInfo">
+              <div><strong>출원일</strong> &nbsp;&nbsp;&nbsp;2022.11.28</div>
+              <div><strong>등록일</strong> &nbsp;&nbsp;&nbsp;2023.05.26.</div>
+            </div>
+            <strong>발명자</strong> &nbsp;&nbsp;&nbsp;반병현<br/>
+            <strong>출원인</strong> &nbsp;&nbsp;&nbsp;농업회사법인 상상텃밭 (주)<br/>
+          </div>
+          <div className="projectIcons">
+            <h5>
+              <span className="citation"  onClick={
+                function(e){
+                  e.preventDefault()
+                  let dummy = document.createElement("textarea");
+                  document.body.appendChild(dummy);
+                  dummy.value = 'Byunghyun Ban, et al. Apparatus and method for providing plant factory digital twin interface using 3D image reconstruction algorithm. KR Patent 10-2538747 (2023)'
+                  dummy.select();
+                  document.execCommand("copy");
+                  document.body.removeChild(dummy);
+                  alert("Citation is copied to Clipboard!")
+                }
+              }>
+                <i className="far fa-clipboard"></i>&nbsp;Cite This
+              </span>
+            </h5>
+            <div className="gitIcons">
+              <a href="https://cdn.jsdelivr.net/gh/needleworm/needleworm.github.io/src/patentPdf/30.pdf" target="_blank"  rel="noreferrer" download>
+                <i className="fas fa-file-pdf"></i>
+              </a>
+              &nbsp;&nbsp;&nbsp;
+              <a href="https://doi.org/10.8080/1020220161821" target="_blank"  rel="noreferrer">
+                <i className="fas fa-external-link-alt"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
     return (
       <div className="websitesContainer">
         {patent29}
